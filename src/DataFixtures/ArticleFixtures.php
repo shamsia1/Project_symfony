4ee3423d33bc7d42285fbd2ebd221e22fea083e0
shamsia1 <alimishamsya@gmail.com>
2019-06-07 22:10:58 +0200

    slugify

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 
 
 use App\Entity\Article;
+use App\Service\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -20,12 +21,14 @@ class ArticleFixtures extends Fixture implements DependentFixtureInterface
 public function load(ObjectManager $manager)
 {
     $faker = Faker\Factory::create('en_US');
+    $slugify= new Slugify();
 
 
     for ($i = 1; $i <= 50; $i++) {
         $article = new article();
         //$article->setTitle('Framework PHP : symfony4' . $i);
         $article->setTitle(mb_strtolower($faker->sentence()));
+        $article->setSlug($slugify->generate($article->getTitle()));
         $article->setContent(mb_strtolower($faker->text));
         $manager->persist($article);
         $article->setCategory($this->getReference('categorie_0'));
